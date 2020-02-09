7b3e93542a9f33dfaa35ed315d5b88a789c26c90
Jitgitbit <rowowolf@gmail.com>
2020-02-09 23:18:28 +0100

    heroku pg addon

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const express = require(`express`);
 const app = express();
 
 const port = process.env.PORT || 4000;
+//Created postgresql-convex-68307 as DATABASE_URL
 
 app.listen(port, () => console.log(`The imageboard-server API is listening on port ${port}!`));
