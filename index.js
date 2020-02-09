79916d8a7f97f0b920a4881774e4983b7acb53d7
Jitgitbit <rowowolf@gmail.com>
2020-02-09 23:37:07 +0100

    basic db set up

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require(`express`);
+const db = require(`./db`);
 
 const app = express();
 
 const port = process.env.PORT || 4000;
-//Created postgresql-convex-68307 as DATABASE_URL
 
 app.listen(port, () => console.log(`The imageboard-server API is listening on port ${port}!`));
