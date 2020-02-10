f11f67a1141e080d6f04a73c5eb52413f2d67199
Jitgitbit <rowowolf@gmail.com>
2020-02-10 14:08:02 +0100

    middleware added for endpoint auth

--- auth/router.js
+++ auth/router.js
@@ -4,6 +4,7 @@ const db = require("../db");
 const Sequelize = require("sequelize");
 const User = require(`../user/model`);
 const bcrypt = require(`bcrypt`);
+const auth = require(`./middleware`)
 
 
 const router = new Router();
@@ -52,28 +53,32 @@ router.post("/login", (req, res, next) => {
     
 });
 
+// router.get('/secret-endpoint', (req, res) => {
+//   const auth = req.headers.authorization && req.headers.authorization.split(' ')
+//   if (auth && auth[0] === 'Bearer' && auth[1]) {
+//     try {
+//       const data = toData(auth[1])
+//       res.send({
+//         message: 'Thanks for visiting the secret endpoint.',
+//         data
+//       })
+//     }
+//     catch(error) {
+//       res.status(400).send({
+//         message: `Error ${error.name}: ${error.message}`,
+//       })
+//     }
+//   }
+//   else {
+//     res.status(401).send({
+//       message: 'Please supply some valid credentials'
+//     })
+//   }
+// })
 
-router.get('/secret-endpoint', (req, res) => {
-  const auth = req.headers.authorization && req.headers.authorization.split(' ')
-  if (auth && auth[0] === 'Bearer' && auth[1]) {
-    try {
-      const data = toData(auth[1])
-      res.send({
-        message: 'Thanks for visiting the secret endpoint.',
-        data
-      })
-    }
-    catch(error) {
-      res.status(400).send({
-        message: `Error ${error.name}: ${error.message}`,
-      })
-    }
-  }
-  else {
-    res.status(401).send({
-      message: 'Please supply some valid credentials'
-    })
-  }
+router.get('/secret-endpoint', auth, (req, res) => {
+  res.send({
+    message: `Thanks for visiting the secret endpoint ${req.user.email}.`,
+  })
 })
-
 module.exports = router
