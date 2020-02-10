72d9cf4be359cd06a096585e90d1c528296816b7
Jitgitbit <rowowolf@gmail.com>
2020-02-10 13:56:50 +0100

    auth fully functional now

--- auth/router.js
+++ auth/router.js
@@ -3,6 +3,8 @@ const { toJWT, toData } = require("./jwt");
 const db = require("../db");
 const Sequelize = require("sequelize");
 const User = require(`../user/model`);
+const bcrypt = require(`bcrypt`);
+
 
 const router = new Router();
 
@@ -12,25 +14,44 @@ const router = new Router();
 //   password: Sequelize.TEXT
 // });
 //CRUD CREATE
-router.post("/login", (request, response, next) => {
-  const { email, password } = request.body;
-  User.create({
-    email: email,
-    password: password
+router.post("/login", (req, res, next) => {
+  // const { email, password } = req.body;
+  User
+  .findOne({
+    where: {
+      email: req.body.email
+    }
   })
-    .then(user => {
-      if (!email || !password) {
-        response.status(400).send({
-          message: "Please supply a valid email and password"
-        });
-      } else {
-        response.send({
-          jwt: toJWT({ userId: 1 })
-        });
-      }
+  .then(entity => {
+    if (!entity) {
+      res.status(400).send({
+        message: 'User with that email does not exist'
+      })
+    }
+    // 2. use bcrypt.compareSync to check the password against the stored hash
+    else if (bcrypt.compareSync(req.body.password, entity.password)) {
+      // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+      console.log(`password is correct`)
+      res.send({
+        message: `password is correct`,
+        jwt: toJWT({ userId: entity.id })
+      })
+    }
+    else {
+      res.status(400).send({
+        message: 'Password was incorrect'
+      })
+    }
+  })
+  .catch(err => {
+    console.error(err)
+    res.status(500).send({
+      message: 'Something went wrong'
     })
-    .catch(error => next(error));
+  })
+    
 });
+
 
 router.get('/secret-endpoint', (req, res) => {
   const auth = req.headers.authorization && req.headers.authorization.split(' ')
--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const Sequelize = require(`sequelize`);
 const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const db = new Sequelize(databaseURL);
 
-// db.sync({force: true }) // to overwrite!
-db.sync()
+db.sync({force: true }) // to overwrite!
+// db.sync()
   .then(console.log(`db synced`))
   .catch(err => {
     console.error(`Table schema fail`, err);         // in case hardcoded data to add and test!   of course a hardcode fn is also needed then!
