615795c7cf756e2319b693f681f4be28dccf48ce
Jitgitbit <rowowolf@gmail.com>
2020-02-10 11:39:45 +0100

    login post works

--- auth/router.js
+++ auth/router.js
@@ -1,8 +1,34 @@
-const { Router } = require('express')
-const { toJWT, toData } = require('./jwt')
+const { Router } = require("express");
+const { toJWT, toData } = require("./jwt");
+const db = require("../db");
+const Sequelize = require("sequelize");
 
-const router = new Router()
+const router = new Router();
 
+// User model
+const User = db.define("User", {
+  email: Sequelize.TEXT,
+  password: Sequelize.TEXT
+});
+//CRUD CREATE
+router.post("/login", (request, response, next) => {
+  const { email, password } = request.body;
+  User.create({
+    email: email,
+    password: password
+  })
+    .then(user => {
+      if (!email || !password) {
+        response.status(400).send({
+          message: "Please supply a valid email and password"
+        });
+      } else {
+        response.send({
+          jwt: toJWT({ userId: 1 })
+        });
+      }
+    })
+    .catch(error => next(error));
+});
 
-// define endpoints here
 module.exports = router
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const db = require(`./db`)
 const Image = require(`./image/model`)
 const Sequelize = require(`sequelize`);
 const imageRouter = require(`./image/router`)
+const authRouter = require(`./auth/router.js`)
 const cors = require(`cors`);
 
 const port = process.env.PORT || 4000;
@@ -17,6 +18,7 @@ const parserMiddleware = express.json()  // body-parser and express have recentl
 app.use(parserMiddleware)
 
 app.use(imageRouter);
+app.use(authRouter);
 
 app.listen(port, () => console.log(`The imageboard-server API is listening on port ${port}!`));
 
