a1503463bbe9bc31f400c957b95144d09fac100f
Jitgitbit <rowowolf@gmail.com>
2020-02-10 14:10:25 +0100

    fully functioning login/signup

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Sequelize = require(`sequelize`);
 const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const db = new Sequelize(databaseURL);
 
-// db.sync({force: true }) // to overwrite!
+// db.sync({force: true }) // to overwrite!!
 db.sync()
   .then(console.log(`db synced`))
   .catch(err => {
